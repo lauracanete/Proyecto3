8e16f30b644008d42198684515a82a385badb8cd
lauracanete <109248893+lauracanete@users.noreply.github.com>
2022-08-26 17:36:22 -0300

    cambio

---
+++ Backend/src/main/java/com/proyectofinal/laura/LauraApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.laura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauraApplication.class, args);
+	}
+
+}
